58b528b28de898de019d5c8193676fdb2b7c83c5
Devo Thomas <devo@ispgweb.com>
2024-03-01 23:20:07 +0000

    lerning redux

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -1,10 +1,21 @@
+import { useSelector, useDispatch } from "react-redux";
+import { decrease_count, increase_count } from "../Redux/count/countSlice";
 function Counter() {
+  const count = useSelector(state => state.count.value);
+  const dispatch = useDispatch();
+
+  const onIncreament = () => {
+    dispatch(increase_count());
+  }
+  const onDecrement = () => {
+    dispatch(decrease_count());
+  };
   return (
     <div className="counter">
       <h2>Counter</h2>
-      <h3>The count is - 0</h3>
-      <button>Increase</button>
-      <button>Decrease</button>
+      <h3>The count is - {count}</h3>
+      <button onClick={onIncreament}>Increase</button>
+      <button onClick={onDecrement}>Decrease</button>
     </div>
   );
 }
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,7 +1,9 @@
+import { useSelector, useDispatch } from "react-redux";
 function Footer() {
+  const color = useSelector(state => state.color.value);
   return (
     <div className="footer">
-      <p>My Redux project footer</p>
+      <p style={{color}}>My Redux project footer</p>
     </div>
   );
 }
